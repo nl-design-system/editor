d22dad43240b1b80f88b4a0815980cd774aa5b30
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-02 13:07:03 +0200

    chore(2848): add single unit test with vitest and playwright

---
+++ packages/website/vitest.config.ts
@@ -0,0 +1,16 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [vanillaExtractPlugin()],
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      instances: [
+        { browser: 'chromium' },
+      ],
+      provider: 'playwright',
+    },
+  },
+})
