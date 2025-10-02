2db8fc899fd60a325e40d9d5dc3bf57e0b6b1665
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-02 13:08:12 +0200

    chore(2848): lint

--- packages/website/src/components/header/Header.test.tsx
+++ packages/website/src/components/header/Header.test.tsx
@@ -1,8 +1,8 @@
-import { expect, test } from 'vitest'
-import { render } from 'vitest-browser-react'
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-react';
 import Header from './Header.tsx';
 
 test('<Header />', async () => {
-  const { getByText } = render(<Header />)
-  await expect.element(getByText('Rich-text editor met editoria11y (PoC)')).toBeInTheDocument()
-})
+  const { getByText } = render(<Header />);
+  await expect.element(getByText('Rich-text editor met editoria11y (PoC)')).toBeInTheDocument();
+});
