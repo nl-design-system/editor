798ba95de8cb90553b9cddac728ede85b178e363
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-06 10:57:52 +0200

    chore: cross env compatability; split build scripts; bundle analyzer

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,12 +1,16 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { analyzer } from 'vite-bundle-analyzer';
 
 export default defineConfig({
   plugins: [
     react(),
     vanillaExtractPlugin({
       identifiers: ({ hash }) => `clippy_${hash}`,
+    }),
+    analyzer({
+      enabled: process.env['ANALYZE_BUNDLE'] === 'true',
     }),
   ],
   resolve: {
