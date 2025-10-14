fece0dab7676993320e24eef60b1620bca3ce64e
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-14 09:28:07 +0200

    chore: ma-theme

--- packages/website/src/main.ts
+++ packages/website/src/main.ts
@@ -4,4 +4,3 @@ import '@fontsource/source-sans-pro/400.css';
 import '@nl-design-system-community/ma-design-tokens/dist/theme.css';
 import '@utrecht/component-library-css/dist/index.css';
 import './components/editor/ClippyEditor.ts';
-import '@nl-design-system-community/ma-design-tokens/dist/variables.css';
