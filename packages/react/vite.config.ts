44744cbc8ec0496867a4bae1228c876cec855af9
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-20 12:17:20 +0200

    chore: react output
    - Set lib fileName
    
    Refs #19

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      fileName: 'index',
+      formats: ['es'],
+    },
+  },
   server: {
     port: 5175,
     strictPort: true,
