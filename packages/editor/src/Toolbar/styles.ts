bf9d7113c2b5204061e029bad12f42bf90fd69b8
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-23 15:48:03 +0200

    feat: initial setup lit combobox

--- packages/editor/src/Toolbar/styles.ts
+++ packages/editor/src/Toolbar/styles.ts
@@ -2,6 +2,7 @@ import { css } from 'lit';
 
 export default css`
   .clippy-toolbar__wrapper {
+    display: flex;
     background-color: var(--ma-color-paars-1);
     border: 1px solid var(--ma-color-paars-8);
     border-radius: 3px;
