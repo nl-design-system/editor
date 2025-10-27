83b3f47d830e816d4c27e458f8360c3908c642dd
timhilhorst <tim.hilhorst@ictu.nl>
2025-10-27 17:07:16 +0100

    feat(24): validate empty heading

--- packages/editor/src/Gutter/index.ts
+++ packages/editor/src/Gutter/index.ts
@@ -1,23 +1,40 @@
+import type { Editor } from '@tiptap/core';
+import { consume } from '@lit/context';
+import numberBadgeStyles from '@nl-design-system-candidate/number-badge-css/number-badge.css?inline';
 import AccessibleIcon from '@tabler/icons/outline/accessible.svg?raw';
 import CloseIcon from '@tabler/icons/outline/x.svg?raw';
-import { html, LitElement } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { html, LitElement, unsafeCSS } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 import { map } from 'lit/directives/map.js';
+import { createRef, ref, type Ref } from 'lit/directives/ref.js';
 import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import type { ValidationError } from '../types/validation.ts';
+import { tiptapContext } from '../context/TiptapContext.ts';
 import { CustomEvents } from '../events';
 import gutterStyles from './styles.ts';
 
 @customElement('clippy-gutter')
 export class Gutter extends LitElement {
-  static override readonly styles = [gutterStyles];
+  static override readonly styles = [gutterStyles, unsafeCSS(numberBadgeStyles)];
   @state()
   private isOpen = false;
+
+  @consume({ context: tiptapContext })
+  @property({ attribute: false })
+  public editor?: Editor;
 
   @state()
   private validationErrors: ValidationError[] = [];
 
+  private dialogRef: Ref<HTMLDialogElement> = createRef();
+
   #toggleOpen = () => {
+    const { value } = this.dialogRef;
+    if (this.isOpen) {
+      value?.close();
+    } else {
+      value?.show();
+    }
     this.isOpen = !this.isOpen;
   };
 
@@ -26,6 +43,22 @@ export class Gutter extends LitElement {
       this.validationErrors = [...this.validationErrors, event.detail];
     }
   };
+
+  #focusNode(item: ValidationError) {
+    try {
+      const { view } = this.editor || {};
+      const nodeDom = view?.nodeDOM?.(item.id) ?? view?.domAtPos(item.id).node;
+      if (nodeDom instanceof HTMLElement) {
+        nodeDom.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+
+      // Set a text selection at the position and focus the view
+      this.editor?.commands.focus(item.id);
+      this.#toggleOpen();
+    } catch (err) {
+      console.error('Cannot scroll to and focus node', err);
+    }
+  }
 
   override connectedCallback() {
     super.connectedCallback();
@@ -38,49 +71,79 @@ export class Gutter extends LitElement {
   }
 
   renderValidationContent() {
-    return html`<ul class="clippy-overlay__list">
+    return html`<ul class="clippy-dialog__list">
       ${this.validationErrors.length > 0
         ? map(
             this.validationErrors,
             (item) =>
-              html`<li class="clippy-overlay__list-item" tabindex="0">"<i>${item.text}</i>"<br />${item.string}</li>`,
+              html` <li class="clippy-dialog__list-item" xmlns="http://www.w3.org/1999/html" tabindex="0">
+                <div class="clippy-dialog__list-item-quote">"<i>${item.textContent}</i>"</div>
+                <div class="clippy-dialog__list-item-message">${item.message}</div>
+                <div class="clippy-dialog__list-item-link">
+                  <utrecht-link
+                    href="https://nldesignsystem.nl/richtlijnen/content/tekstopmaak/koppen/#voor-wie-zijn-toegankelijke-koppen-belangrijk"
+                    external="true"
+                  >
+                    Uitgebreide toelichting
+                  </utrecht-link>
+                </div>
+                <div class="clippy-dialog__list-item-actions">
+                  <utrecht-button disabled="true">Negeren</utrecht-button>
+                  <utrecht-button appearance="secondary-action-button" @click=${() => this.#focusNode(item)}
+                    >Aanpassen</utrecht-button
+                  >
+                </div>
+              </li>`,
           )
-        : html`<li class="clippy-overlay__list-item">Geen toegankelijkheidsfouten gevonden.</li>`}
+        : html`<li class="clippy-dialog__list-item">Geen toegankelijkheidsfouten gevonden.</li>`}
     </ul>`;
   }
 
   override render() {
-    return html`<div>
+    return html`
       <ol class="clippy-gutter-list" role="list">
         ${map(
           this.validationErrors,
-          (item) =>
+          ({ domRect, message }) =>
+            domRect &&
             html`<li
               class="clippy-gutter-item"
-              style="inset-block-start: ${item.offsetTop}px; block-size: ${item.offsetHeight}px"
-              title=${item.string}
+              style="inset-block-start: ${domRect.top}px; block-size: ${domRect.height}px"
+              title=${message}
             ></li>`,
         )}
       </ol>
       <button
-        class="clippy-overlay-toggle"
+        class="clippy-dialog-toggle"
         @click=${this.#toggleOpen}
         aria-expanded=${this.isOpen}
-        aria-controls="overlay-content"
+        aria-controls="dialog-content"
       >
+        <span class="clippy-screen-reader-text">Toon toegankelijkheidsfouten</span>
         ${this.isOpen ? unsafeSVG(CloseIcon) : unsafeSVG(AccessibleIcon)}
+        ${this.validationErrors.length > 0
+          ? html`<data value=${this.validationErrors.length} class="nl-number-badge nl-number-badge--clippy">
+              <span hidden aria-hidden="true" class="nl-number-badge__visible-label"
+                >${this.validationErrors.length}</span
+              >
+              <span class="nl-number-badge__hidden-label"
+                >${this.validationErrors.length} toegankelijkheidsmeldingen</span
+              >
+            </data>`
+          : null}
       </button>
-      ${this.isOpen
-        ? html`<div
-            id="overlay-content"
-            class="clippy-overlay__content"
-            role="region"
-            aria-label="Toegankelijkheidsfouten"
-          >
-            ${this.renderValidationContent()}
-          </div>`
-        : null}
-    </div>`;
+      <div class="clippy-screen-reader-text" aria-live=${this.validationErrors.length > 0 ? 'polite' : 'off'}>
+        Totaal ${this.validationErrors.length} gevonden toegankelijkheidsfouten.
+      </div>
+      <dialog
+        ${ref(this.dialogRef)}
+        id="dialog-content"
+        class="clippy-dialog__content"
+        aria-label="Toegankelijkheidsfouten"
+      >
+        ${this.renderValidationContent()}
+      </dialog>
+    `;
   }
 }
 
--- packages/editor/src/validation/index.ts
+++ packages/editor/src/validation/index.ts
@@ -1,39 +1,91 @@
+import type { Node } from 'prosemirror-model';
 import { type Editor, Extension } from '@tiptap/core';
 import type { ValidationError } from '../types/validation.ts';
 import { CustomEvents } from '../events';
+import { debounce } from '../utils/debounce.ts';
 
-class ValidationOptions {}
+type Validator = (node: Node, pos: number, editor: Editor) => ValidationError[];
 
-const validateDocument = (editor: Editor) => {
-  console.time('validation');
-  const $headings = editor.$nodes('heading');
-  if ($headings?.length) {
-    for (const { pos } of $headings) {
-      const { node } = editor.view.domAtPos(pos);
-      if (node instanceof HTMLElement) {
-        const payload: ValidationError = {
-          id: pos,
-          offsetHeight: node.offsetHeight,
-          offsetTop: node.offsetTop,
-          string: 'Mock heading validation error',
-          text: node.innerText,
-        };
-        window.dispatchEvent(new CustomEvent(CustomEvents.VALIDATION_ERROR, { detail: payload }));
-      }
-    }
+const validators = new Map<string, Validator[]>();
+
+const getDomRect = (editor: Editor, pos: number): DOMRect | null => {
+  const domNode = editor.view.nodeDOM(pos);
+  if (domNode instanceof HTMLElement) {
+    return domNode.getClientRects()[0];
   }
-  console.timeEnd('validation');
-  return [];
+  return null;
 };
 
-export default Extension.create<ValidationOptions>({
+export function registerValidator(nodeType: string, v: Validator) {
+  const arr = validators.get(nodeType) ?? [];
+  arr.push(v);
+  validators.set(nodeType, arr);
+}
+
+const isEmptyOrWhitespaceString = (str: string): boolean => /^\s*$/.test(str);
+
+const isEmpty = (node: Node): boolean => {
+  if (node.type.name === 'text') {
+    return !node.text || isEmptyOrWhitespaceString(node.text);
+  } else if (node.content?.content) {
+    return node.content?.content.every((node) => isEmpty(node));
+  }
+  return true;
+};
+
+registerValidator('heading', (node, pos, editor) => {
+  const textContent = node.textContent ?? '';
+
+  if (isEmpty(node)) {
+    const payload: ValidationError = {
+      id: pos,
+      domRect: getDomRect(editor, pos),
+      message: 'Heading must not be empty',
+      textContent,
+    };
+    return [payload];
+  }
+
+  return [];
+});
+
+const runValidation = (editor: Editor) => {
+  const errors: ValidationError[] = [];
+
+  editor.state.doc.descendants((node, pos) => {
+    const nodeValidators = validators.get(node.type.name);
+    if (!nodeValidators || nodeValidators.length === 0) return true;
+    for (const v of nodeValidators) {
+      try {
+        const result = v(node, pos, editor);
+        if (result && result.length) {
+          errors.push(...result);
+        }
+      } catch (err) {
+        console.error('validator error', err);
+      }
+    }
+
+    return true;
+  });
+
+  for (const payload of errors) {
+    window.dispatchEvent(new CustomEvent(CustomEvents.VALIDATION_ERROR, { detail: payload }));
+  }
+
+  return errors;
+};
+
+const debouncedValidate = debounce(runValidation, 150);
+
+export default Extension.create({
   name: 'validation',
 
-  // onCreate({ site }) {
-  //   validateDocument(site);
-  // },
+  onCreate({ editor }) {
+    debouncedValidate(editor);
+  },
 
   onUpdate({ editor }) {
-    validateDocument(editor);
+    debouncedValidate(editor);
   },
 });
