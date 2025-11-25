c954e15bb22391bbe853d5d1174d5ecafaf7de55
timhilhorst <tim.hilhorst@ictu.nl>
2025-11-25 09:17:59 +0100

    chore: basic playwright test

--- packages/editor-website/e2e/page.spec.ts
+++ packages/editor-website/e2e/page.spec.ts
@@ -3,6 +3,18 @@ import { test, expect } from '@playwright/test';
 test('Page has correct title', async ({ page }) => {
   await page.goto('http://localhost:5174/');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Rich-text Editor - NL Design System');
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('paragraph').filter({ hasText: /^$/ }).fill('Paragraaf.');
+  await page.getByRole('listitem').nth(5).click();
+  await page.getByRole('button', { name: 'Aanpassen' }).first().click();
+  await page.getByRole('listitem').nth(3).click();
+  await page.getByRole('button', { name: 'Aanpassen' }).first().click();
+  await page.getByRole('heading', { name: 'Dat mag niet, he' }).click();
+  await page.waitForTimeout(100); // Wait for selector
+  await page.getByRole('combobox').selectOption('h3');
+  expect(page.getByRole('heading', { level: 3 })).toBeDefined();
+  await page.getByLabel('Werkbalk tekstbewerker').getByText('1', { exact: true }).click();
+  page.getByText('Lijst moet een semantische lijst zijn');
+  await expect(page.getByRole('button', { name: 'Aanpassen' })).toHaveCount(1);
 });
