8e8980b6ad59e580f2f2bcedb06908d480ad96fc
Bart Veneman <bartveneman@gmail.com>
2025-12-08 14:06:38 +0100

    fix pw baseurl?

--- packages/editor-website/e2e/page.spec.ts
+++ packages/editor-website/e2e/page.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Page has correct title', async ({ page }) => {
-  await page.goto('http://localhost:5174/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Rich-text Editor - NL Design System');
--- packages/editor-website/playwright.config.ts
+++ packages/editor-website/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   testDir: './e2e',
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5174',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
