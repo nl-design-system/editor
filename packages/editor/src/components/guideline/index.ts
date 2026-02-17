e02a2f0941bf82f2d5cb3a6d7cc3d0807edd28f5
timhilhorst <tim.hilhorst@ictu.nl>
2026-02-17 08:48:14 +0100

    feat: refactor guideline component to use classMap and unsafeSVG for improved rendering

--- packages/editor/src/components/guideline/index.ts
+++ packages/editor/src/components/guideline/index.ts
@@ -1,5 +1,9 @@
+import MoodHappyIcon from '@tabler/icons/outline/mood-happy.svg?raw';
+import MoodSadIcon from '@tabler/icons/outline/mood-sad.svg?raw';
 import { LitElement, html, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import guidelineStyles from './styles';
 
 @customElement('clippy-guideline')
@@ -12,51 +16,21 @@ export class ClippyGuideline extends LitElement {
   override render() {
     const { type } = this;
     return html`
-      <div class=${`clippy-guideline ${type ? `clippy-guideline--${type}` : ''}`}>
+      <div
+        class=${classMap({
+          [`clippy-guideline--${type}`]: !!type,
+          'clippy-guideline': true,
+        })}
+      >
         <div class="clippy-guideline__description">
-          <h2 class=${`clippy-guideline__badge ${type ? `clippy-guideline__badge--${type}` : ''}`}>
-            ${type === 'positive'
-              ? html`<svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="tabler-icon tabler-icon-mood-happy nlds-guideline__icon"
-                  aria-hidden="true"
-                  role="presentation"
-                >
-                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                  <path d="M9 9l.01 0"></path>
-                  <path d="M15 9l.01 0"></path>
-                  <path d="M8 13a4 4 0 1 0 8 0h-8"></path>
-                </svg>`
-              : nothing}
-            ${type === 'negative'
-              ? html`<svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="tabler-icon tabler-icon-mood-sad clippy-guideline__icon"
-                  aria-hidden="true"
-                  role="presentation"
-                >
-                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                  <path d="M9 10l.01 0"></path>
-                  <path d="M15 10l.01 0"></path>
-                  <path d="M9.5 15.25a3.5 3.5 0 0 1 5 0"></path>
-                </svg>`
-              : nothing}
+          <h2
+            class=${classMap({
+              [`clippy-guideline__badge--${type}`]: !!type,
+              'clippy-guideline__badge': true,
+            })}
+          >
+            ${type === 'positive' ? unsafeSVG(MoodHappyIcon) : nothing}
+            ${type === 'negative' ? unsafeSVG(MoodSadIcon) : nothing}
             <span class="clippy-guideline__title"><slot name="heading"></slot></span>
           </h2>
           <slot name="description"></slot>
