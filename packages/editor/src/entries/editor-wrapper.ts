4d58087c61fc14edfbc01b4a062f09f40a9f1608
Wouter Lenting <wouter.lenting@ictu.nl>
2026-07-20 17:29:30 +0200

    feat: expose editor-wrapper layout components as a package entry point

---
+++ packages/editor/src/entries/editor-wrapper.ts
@@ -0,0 +1,6 @@
+import '../components/editor-wrapper';
+import '../components/editor-content-wrapper';
+
+export { EditorWrapper } from '../components/editor-wrapper';
+export type { DrawerPosition } from '../components/editor-wrapper';
+export { EditorContentWrapper } from '../components/editor-content-wrapper';
