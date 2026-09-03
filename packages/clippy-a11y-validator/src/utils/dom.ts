7e2c9f5341cd57fcb921a7c3cabe818fabc8fafb
timhilhorst <tim.hilhorst@ictu.nl>
2026-09-03 16:33:33 +0200

    chore: sonar

--- packages/clippy-a11y-validator/src/utils/dom.ts
+++ packages/clippy-a11y-validator/src/utils/dom.ts
@@ -16,5 +16,5 @@ export const unwrapElement = (element: Element): void => {
   const parent = element.parentNode;
   if (!parent) return;
   while (element.firstChild) parent.insertBefore(element.firstChild, element);
-  parent.removeChild(element);
+  element.remove();
 };
